84e5cb94173cd8ca00178b516970c04a16b6e619
Nishant Singh <nishant132@users.noreply.github.com>
2018-01-22 22:49:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+    <head>
+    <title> KiiTHelper | EVERYTHING AROUND KIIT YOU ARE ONE STEP AWAY  </title>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="keywords" content="jquery, mega "/>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+         <link href="https://plus.google.com/115947806301160729465" rel="publisher" />    
+    <meta property="og:url" content="http://www.kiithelper.in/" />
+<meta property="og:image" content="http://www.kiithelper.in/kiithelper2.jpg" />
+<meta property="og:title" content="KiiTHelper" />
+<meta property="og:description" content=" Hey Students ever faced difficulty in finding restaurants around KIIT or were you ever stuck in finding a place to visit with family and friends around KIIT or did you waste your time during the exam week running after the teachers,friends and librarian for the question papers.. KiiTHelper is the solution for all your problems..
+	" />
+    <script type="text/javascript" async defer
+  src="https://apis.google.com/js/platform.js?publisherid=115947806301160729465">
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83668591-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700|Candal|Source+Sans+Pro|Hind:500|Fredoka+One' rel='stylesheet' type='text/css'>
+           <link rel="shortcut icon" href="../favicon.ico"> 
+        <link rel="stylesheet" type="text/css" href="files/css1/index.css" />
+        <link rel="stylesheet" type="text/css" href="files/css1/style1.css" />
+        	<link rel="stylesheet" type="text/css" href="files/css1/font-awesome.css">
+            <link rel="stylesheet" type="text/css" href="files/css/font-awesome.css">
+            	<script type="text/javascript" src="files/js/jquery.js"></script>
+	            <script type="text/javascript" src="files/js/function.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="files/css1/header.css">
+    	<link rel="stylesheet" type="text/css" href="files/css1/footer.css">
+   	<link rel="stylesheet" type="text/css" href="files/css1/search.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   
+		<script type="text/javascript" src="files/js/modernizr.custom.86080.js"></script>
+<style type="text/css">       
+        .input {
+    position: relative;
+}
+
+.tooltip {
+    display: none;
+    padding: 2px;
+   
+}
+
+.input:hover .tooltip {
+    background: rgb(40, 121, 148);
+    border-radius: 3px;
+    bottom: -30px;
+    color: white;
+    display: inline;
+    height: 30px;
+    left: 0;
+    line-height: 30px;
+    position: absolute;
+}
+
+.input:hover .tooltip:before {
+    display: block;
+    content: "";
+    position: absolute;
+    top: -5px;
+    width: 0; 
+    height: 0; 
+    border-left: 5px solid transparent;
+    border-right: 5px solid transparent;
+    border-bottom: 5px solid blue;
+}
+        
+ </style>  
+    </head>
+    <body>
+   <div id="wrap">
+	<header>
+		<div class="inner relative">
+			<a class="logo" href="index.php"><img src="files/images/my_logo8.png" alt="KiiTHelper"></a>
+			<a style="height: 100%;width: 100%;margin-top: 36px; box-sizing: border-box;height: 19px;width: 24px;background: #fff; border-radius: 4px;text-align: center;font-weight: 900px;" id="menu-toggle" class="button dark" href="#"><i class="icon-reorder"></i></a>
+			<nav id="navigation">
+				<ul id="main-menu">
+					<li class="current-menu-item" >
+                    <a href="index.php" style="margin-top: 26px;"><img src="files/images/header_menu_home_icon.png" ></a>
+                    </li>
+                    	<li><a href="files/aboutus.php">About Us</a></li>
+					<li class="parent">
+						<a href="files/qp.php" alt="Study Portal"  title="Study Portal">Study Portal</a>
+						<ul class="sub-menu">
+                        	<li><a href="files/qp.php"><i class="icon-book"></i>Question Papers</a></li>
+							<li><a href="files/placementhelper.php"><i class="icon-wrench"></i>Placement Helper</a></li>
+							
+						</ul>
+					</li>
+					<li class="parent">
+						<a href="#" alt="Restaurants" title="Restaurants">Restaurants</a>
+						<ul class="sub-menu">
+							<li><a href="files/restaurants.php"><i class="fa fa-cutlery"></i>&nbsp;Restaurants</a></li>
+							<li><a href="files/guesthouse.php"><i class="fa fa-bed"></i>&nbsp;Guest House</a></li>
+						</ul>
+					</li>
+                    	<li class="parent">
+						<a href="#" alt="Funzone" title="Funzone">Funzone</a>
+						<ul class="sub-menu">
+							<li><a href="files/placetovisit.php"><i class="fa fa-university"></i>&nbsp;Places to visit</a></li>
+							<li><a href="files/mall&movie.php"><i class="fa fa-video-camera"></i>&nbsp;Mall and Theatres</a></li>
+						</ul>
+					</li>
+					<li class="parent">
+						<a target="_blank" href="http://localhost/blog/#/">Blog</a>
+						
+					</li>
+                    <li class="parent">
+						<a href="content/login.php"><i class="fa fa-upload"></i>&nbsp;Upload&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>
+						
+					</li>
+				
+                    <li class="signup1"><a href="content/register.php" ><b style="box-sizing: border-box;padding-left: 11px;padding-right: 11px;height: 20px; color: #fff; width: 33px;padding-top: 7px;margin-right: 2px; padding-bottom: 7px; border-radius: 4px;  color: #fff;text-shadow: 0 -1px 0 rgba(0,0,0,0.15);background-color: #287994;background-image: -webkit-linear-gradient(#287994, #2B8BAB);background-image: linear-gradient(#287994, #2B8BAB);border-color: #065670;"><span style="color: #fff;">Sign up&nbsp;<i class="fa fa-user-plus"></i></span></b></a></li>
+                    <li style="margin-left: 4px;" ><a href="content/login.php"><b style="box-sizing: border-box;padding-left: 11px;margin-left: 4px; padding-right: 11px;height: 19px; color: #fff; width: 24px;padding-top: 6px;padding-bottom: 7px; border-radius: 4px;  color: #fff;text-shadow: 0 -1px 0 rgba(0,0,0,0.15);background-color: #ecf0f1;background-image: -webkit-linear-gradient(#ecf0f1, #bdc3c7);background-image: linear-gradient(#ecf0f1, #bdc3c7);border-color: #7f8c8d;"><span style="color: #2c3e50;">Sign in&nbsp;<i class="fa fa-sign-in"></i></span></b></a></li>
+				</ul>
+			</nav>
+			<div class="clear"></div>
+		</div>
+	</header>	
+</div>    
+  
+<div id="page">
+        <ul class="cb-slideshow">
+            <li><span>Image 01</span><div></div></li>
+            <li><span>Image 02</span><div></div></li>
+            <li><span>Image 03</span><div></div></li>
+            <li><span>Image 04</span><div></div></li>
+            <li><span>Image 05</span><div></div></li>
+            <li><span>Image 06</span><div></div></li>
+        </ul>
+        <div class="container">
+            <h1 style="text-align:center;left: 190px;font-family: Montserrat:700;" > <span>EVERYTHING IN AND AROUND KIIT<br />YOU ARE JUST ONE STEP AWAY</span></h1>
+             </div>
+            <section class="codrops-top">
+                 <ul id="ldd_menu" class="ldd_menu">
+				<li>
+					<span>Restaurants</span>
+					<div class="ldd_submenu">
+						<ul>
+							<li class="ldd_heading">By Category</li>
+							<li><a href="files/budgethotel.php">Budget</a></li>
+							<li><a href="files/reasonablehotel.php">Reasonable</a></li>
+							
+							
+						</ul>
+						<ul>
+							<li class="ldd_heading">By Location</li>
+							<li><a href="files/kiitroad.php">KIIT Road</a></li>
+							<li><a href="files/infocityarea.php">Infocity Area</a></li>
+							<li><a href="files/patia.php">Patia</a></li>
+							
+						</ul>
+						<a class="ldd_subfoot" href="files/restaurants.php"> + View All</a>
+					</div>
+				</li>
+				<li>
+					<span>Funzone</span>
+					<div class="ldd_submenu">
+						<ul>
+							<li class="ldd_heading">By Location</li>
+							<li><a href="files/bhubaneswar.php">Bhubaneswar</a></li>
+							<li><a href="files/others.php">Others</a></li>
+                            
+						</ul>
+						<ul>
+							<li class="ldd_heading">By Category</li>
+							<li><a href="files/mall&movie.php">Mall and Movie</a></li>
+							<li><a href="files/placetovisit.php">Tourist Place</a></li>
+                            
+						</ul>
+						<a class="ldd_subfoot" href="files/placetovisit.php"> + View All</a>
+					</div>
+				</li>
+				<li>
+					<span>Study Portal</span>
+					<div class="ldd_submenu">
+						<ul>
+							<li class="ldd_heading">Question Papers</li>
+							<li><a href="files/qp.php">CS and IT</a></li>
+							<li><a href="files/qp.php">EEE and ETC</a></li>
+							<li><a href="files/qp.php">Mech and Civil</a></li>
+						</ul>
+						<ul>
+							<li class="ldd_heading">Placement Helper</li>
+							<li><a href="files/placementhelper.php">Technical</a></li>
+							<li><a href="files/placementhelper.php">English</a></li>
+							<li><a href="files/placementhelper.php">Apptitude</a></li>
+							
+				
+						</ul>
+						<a class="ldd_subfoot" href="files/placementhelper.php"> + View All</a>
+					</div>
+				</li>
+			</ul>
+
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(function() 
+            {
+				var $menu = $('#ldd_menu');
+				
+				$menu.children('li').each(function(){
+					var $this = $(this);
+					var $span = $this.children('span');
+					$span.data('width',$span.width());
+					
+					$this.bind('mouseenter',function(){
+						$menu.find('.ldd_submenu').stop(true,true).hide();
+						$span.stop().animate({'width':'400px'},300,function(){
+							$this.find('.ldd_submenu').slideDown(300);
+						});
+					}).bind('mouseleave',function(){
+						$this.find('.ldd_submenu').stop(true,true).hide();
+						$span.stop().animate({'width':$span.data('width')+'px'},300);
+					});
+				});
+            });
+        </script>	
+               
+            </section>
+            
+            <section class="test">
+            
+            <div class="aboutUs__secondBox">
+
+<div class="aboutUs__quarterBox">
+ <a href="files/restaurants.php" >
+			<div class="imageDiv imageDiv--first">
+			</div>
+            
+			<div class="content">
+				<h5>Around KIIT</h5>
+					<p style=" box-sizing: border-box;display: block;font-family: 'Hind', sans-serif;font-size:14px ;overflow-x: hidden;overflow-y: hidden; height:88px;line-height:22px ;margin-top:0px;text-align:center ;width:255px;">
+			To help Kiitians find and connect with great places to eat,shop and many more things around them.
+				</p >
+			</div>
+            </a>
+		</div>
+
+<div class="aboutUs__quarterBox">
+
+<a href="files/qp.php" >
+			<div class="imageDiv imageDiv--second" >
+			
+			</div>
+           
+			<div class="content">
+				<h5>Service</h5>
+				<p style=" box-sizing: border-box;display: block;font-family: 'Hind', sans-serif;font-size:14px ;overflow-x: hidden;overflow-y: hidden; height:88px;line-height:22px ;margin-top:0px;text-align:center ;width:255px;">
+	Helps student in matters of day to day study, university exams and placements.
+			
+				</p >
+			</div>
+             </a>
+		</div>
+       
+        <div class="aboutUs__quarterBox">
+      <a href="files/placetovisit.php" style="">
+			<div class="imageDiv imageDiv--third">
+			
+			</div>
+            </a>
+			<div class="content">
+				<h5>FunZone</h5>
+					<p style=" box-sizing: border-box;display: block;font-family: 'Hind', sans-serif;overflow-x: hidden;overflow-y: hidden;font-size:14px ;height:88px;line-height:22px ;margin-top:0px;text-align:center ;width:255px;">
+	Banged your head to decide where to go on holidays, worry no more.KiiTHelper is the Solution.
+	
+				</p >
+			</div>
+             </a>
+		</div>
+</div>
+            </section>
+           <section class="foot">
+           	<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>KiiT<span>Helper</span></h3>
+
+				<p class="footer-links" style="font-family: 'Montserrat', sans-serif;">
+					<a href="index.php">Home</a>
+					·
+					<a href="files/restaurants.php">Restaurants</a>
+					·
+					<a href="files/aboutus.php">About Us</a>
+					·
+					<a href="files/mall&movie.php">Funzone</a>
+					·
+				</p>
+
+				<p class="footer-company-name">KiiTHelper &copy; 2016</p>
+
+				<div class="footer-icons">
+
+					<a href="https://www.facebook.com/kiithelper.IND"><img src="files/images/fblogo.png"/> <i class="fafacebook"></i></a>
+					<a href="https://twitter.com/KiiTHelper"><img src="files/images/ttlogo.png"/><i class="fatwitter"></i></a>
+					<a href="https://www.linkedin.com/nhome/"><i class="fagithub"><img src="files/images/liin.png"/></i></a>
+                    <a href="https://plus.google.com/u/0/b/103914821924545868457/103914821924545868457/posts/p/pub?_ga=1.103148160.1828233034.1455224524"><img src="files/images/gplus.png"/></i></a>
+
+
+				</div>
+			</div>
+	<div class="footer-right">
+            			<p>Contact Us</p>
+				<form action="content/login.php " method="post">
+                <div class="input">
+                <input type="text" name="text" placeholder="Name" />
+                <span class="tooltip">You have to login </span>
+                </div>
+
+					<input type="email" name="email" placeholder="Email" />
+                    
+					<textarea name="message" placeholder="Message"></textarea>
+                    
+					<button>Send</button>
+
+				</form>
+
+			</div>
+		</footer>
+
+           
+           
+           
+           
+           </section> 
+             
+        </div>
+  
+        
+    </body>
+</html>
